9f825035aafcf9cd695eaaaf1f691337ecb48981
ketan shah <shahketan1810@gmail.com>
2021-04-22 15:37:11 +0530

    api not working lol

--- client/src/components/Messages/Message/Message.js
+++ client/src/components/Messages/Message/Message.js
@@ -4,6 +4,7 @@ import './Message.css';
 
 
 const Message = ({ message: { text, user }, name }) => {
+
   let isSentByCurrentUser = false;
   let isAdmin = false;
 
@@ -15,6 +16,57 @@ const Message = ({ message: { text, user }, name }) => {
 
   if(user === 'admin' || user==='Admin'){
     isAdmin = true;
+  }
+
+  if(text[0]==='!'){
+    let command= text.slice(1,5);
+    if(command==='stop'){
+     //do something 
+    }
+    else{
+      let song = text.slice(6);
+      let admintext = `'${song}' requested by ${user}`;
+      song = song.split(' ').join('+');
+      async function getURL(){
+        let ans = await fetch(`https://www.googleapis.com/youtube/v3/search?q=${song}&key=AIzaSyAPIDZmHadGqC2Jje5_NO_36Z6UBS4dlTY`);
+        let sol = await ans.json();
+        let url = `https://www.youtube.com/watch?v=${sol.items[0].id.videoId}`;
+        return url;
+      }
+      let link = getURL();
+      console.log(link);
+      if(isSentByCurrentUser){
+        return (
+          <div>
+            <div className="messageContainer justifyEnd">
+              <div className="messageBox backgroundDark">
+                <p className="messageText colorWhite">{text}</p>
+              </div>
+            </div>
+            <div className="adminContainer">
+                <p className="adminText">{admintext}</p>
+            </div>
+            <div className="adminContainer">
+            </div>
+          </div>
+        );
+      }
+      else{
+        return (
+          <div>
+            <div className="messageContainer justifyStart">
+              <div className="messageBox backgroundBlue">
+                <p className="messageText colorWhite">{text}</p>
+              </div>
+              <p className="sentText pl-10 ">{user}</p>
+            </div>
+            <div className="adminContainer">
+                <p className="adminText">{admintext}</p>
+            </div>
+          </div>
+        );
+      }
+    }
   }
 
   return (
